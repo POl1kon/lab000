6476cce226542350da14c313730e6af186953d7d
POl1kon <shm051006@yandex.ru>
2025-09-16 16:10:33 +0300

    commit 3

--- laba01/laba01.cpp
+++ laba01/laba01.cpp
@@ -4,7 +4,7 @@ struct Tube {
 	string name;
 	float dlina;
 	int diametr;
-	bool check;
+	bool sost;
 };
 
 struct Ks {
@@ -18,6 +18,11 @@ void menu() {
 
 
 	while (true) {
+
+		Tube T;
+		Ks K;
+
+
 		cout << "1. Добавить трубу" << endl;
 		cout << "2. Добавить КС" << endl;
 		cout << "3. Просмотр всех объектов" << endl;
@@ -26,13 +31,54 @@ void menu() {
 		cout << "6. Сохранить данные" << endl;
 		cout << "7. Загрузить данные" << endl;
 		cout << "8. Выход" << endl;
-		break;
+
+		int number;
+		cin >> number;
+		cout << "\n";
+
+		switch (number) {
+		case 1:
+		{
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		case 3:
+		{
+			break;
+		}
+		case 4:
+		{
+			break;
+		}
+		case 5:
+		{
+			break;
+		}
+		case 6:
+		{
+			break;
+		}
+		case 7:
+		{
+			break;
+		}
+		case 8:
+			return;
+		default:
+		{
+			cout << "Данная команда отсутствует\n";
+			break;
+		}
+
+		}
 	}
 }
 
 int main() {
-	Tube T;
-	Ks K;
+	setlocale(LC_ALL, "Rus");
 	menu();
 	return 0;
 }
